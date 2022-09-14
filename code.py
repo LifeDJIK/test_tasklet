cd1d5519ea62af75fa0dad5986e4ae4e7fa24f37
Ivan Krakhmaliuk <ivan_krakhmaliuk@epam.com>
2022-09-14 21:50:18 +0300

    Test code

---
+++ code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding=utf-8
+""" Task """
+
+from pylon.core.tools import log  # pylint: disable=E0401
+
+
+def execute(context, cleanup_directories):  # pylint: disable=R0914,R0912,R0915
+    """ Task entry point """
+    _ = context, cleanup_directories
+    #
+    log.info("Tasklet code started")
