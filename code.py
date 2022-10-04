e5e3a063a975f4b3b00941fef7d34d8ca602cacb
Ivan Krakhmaliuk <ivan_krakhmaliuk@epam.com>
2022-10-04 22:14:57 +0300

    Test: exception

--- code.py
+++ code.py
@@ -12,4 +12,6 @@ def execute(context, cleanup_directories):  # pylint: disable=R0914,R0912,R0915
     log.info("Tasklet code started")
     log.info("Args: %s", context.args)
     #
+    raise ValueError("My fault")
+    #
     return 42
