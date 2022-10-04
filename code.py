141bb1c5b2ebaab5890c25fa02679c6ef03769b3
Ivan Krakhmaliuk <ivan_krakhmaliuk@epam.com>
2022-10-04 22:05:58 +0300

    Test: return

--- code.py
+++ code.py
@@ -11,3 +11,5 @@ def execute(context, cleanup_directories):  # pylint: disable=R0914,R0912,R0915
     #
     log.info("Tasklet code started")
     log.info("Args: %s", context.args)
+    #
+    return 42
